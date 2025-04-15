1994abf7bad76764888f460836160b6f0d8f2bd4
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2025-04-15 17:27:02 -0500

    INVENTORY WORKING

--- CapstoneGame/Assets/Scripts/ItemDragHandler.cs
+++ CapstoneGame/Assets/Scripts/ItemDragHandler.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ItemDragHandler : MonoBehaviour
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+
     void Start()
     {
-        
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent;
+        transform.SetParent(transform.root);
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        transform.position = eventData.position; // follow mouse
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+        if (dropSlot == null)
+        {
+            GameObject dropItem = eventData.pointerEnter;
+            if (dropItem != null)
+            {
+                dropSlot = dropItem.GetComponentInParent<Slot>();
+            }
+        }
+        Slot originalSlot = originalParent.GetComponent<Slot>();
+
+        if (dropSlot != null)
+        {
+            //slot under drop point
+            if (dropSlot.currentItem != null)
+            {
+
+                //swap items in slots
+                dropSlot.currentItem.transform.SetParent(originalParent.transform);
+                originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+            else
+            {
+                originalSlot.currentItem = null;
+            }
+
+            // move item into slot
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            transform.SetParent(originalParent); // No slot
+        }
+
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
 }
