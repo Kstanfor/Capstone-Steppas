432ad034565e509046ed0e55c4d5c7752ee47daf
Quetzalberry <143647124+Quetzalberry@users.noreply.github.com>
2025-04-26 20:48:47 -0500

    Eye Enemy Animation

--- CapstoneGame/Assets/Scripts/EnemyScript.cs
+++ CapstoneGame/Assets/Scripts/EnemyScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -15,6 +16,8 @@ public class EnemyScript : MonoBehaviour
     public float StartPositionX;
     public float StartPositionY;
 
+    Vector3 FlipSprite;
+
     void Update()
     {
         if (PlayerDetected == true)
@@ -23,10 +26,23 @@ public class EnemyScript : MonoBehaviour
             Vector2 direction = Player.transform.position - transform.position;
 
             transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, Speed * Time.deltaTime);
+
+            FlipSprite = Player.transform.position;
         }   
         else if(PlayerDetected == false)
         {
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(StartPositionX, StartPositionY), Speed * Time.deltaTime);
+
+            FlipSprite = new Vector3(StartPositionX, StartPositionY, transform.position.z);
+        }
+
+        if (FlipSprite.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-3, 3, 3); 
+        }
+        else if (FlipSprite.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(3, 3, 3);
         }
     }
 
