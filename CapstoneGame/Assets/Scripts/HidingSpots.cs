d9cb809252451edea2e8f7f8084d39b2eb8afc9b
Quetzalberry <143647124+Quetzalberry@users.noreply.github.com>
2025-05-01 11:39:11 -0500

    Animated Hiding Spot

--- CapstoneGame/Assets/Scripts/HidingSpots.cs
+++ CapstoneGame/Assets/Scripts/HidingSpots.cs
@@ -6,6 +6,38 @@ public class HidingSpots : MonoBehaviour
 {
     public bool isTouchingPlayer = false;
 
+    public bool ShouldAnimate = false;
+
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var HidePlayer = animator.GetBool("IsPlayerHiding");
+
+        if (Input.GetKey(KeyCode.E) && isTouchingPlayer == true)
+        {
+            ShouldAnimate = true;
+        }
+        else
+        {
+            ShouldAnimate = false;
+        }
+
+        if(ShouldAnimate == true)
+        {
+            animator.SetBool("IsPlayerHiding", true);
+        }
+        else if(ShouldAnimate == false)
+        {
+            animator.SetBool("IsPlayerHiding", false);
+        }
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
